61d8eedfb3c6374005f2205f97440fd743e7f124
sandervanhooft <sandervanhooft@users.noreply.github.com>
2023-08-28 20:45:39 +0000

    Fix styling

--- tests/Endpoints/AutoPaginatorTest.php
+++ tests/Endpoints/AutoPaginatorTest.php
@@ -157,7 +157,7 @@ class AutoPaginatorTest extends BaseEndpointTest
 
         /** @var CustomerCollection $customers */
         $customers = $this->client->customers->page(
-            ending_before: 'customer_78b146a7de7d417e9d68d7e6ef193d24', 
+            ending_before: 'customer_78b146a7de7d417e9d68d7e6ef193d24',
             limit: 2
         );
 
